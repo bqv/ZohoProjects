fe3c1b7704be3adba1be7e0d51fc0b7e65761ab1
Tony Olagbaiye <frony0@gmail.com>
2018-11-06 20:24:47 +0000

    Base

---
+++ ZohoProjects/CommonIncludes.h
@@ -0,0 +1,14 @@
+﻿// CommonIncludes.h
+
+#pragma once
+
+#include "Resource.h"
+#include "..\ZohoProjectsUI\Resource.h"
+#include "Guids.h"
+#include "..\ZohoProjectsUI\CommandIds.h"
+
+#include "ZohoProjects.h"
+
+#include "Package.h"
+
+
