7e4a3fee379f331f556a158975fca109fd5a4443
Tony Olagbaiye <frony0@gmail.com>
2018-11-10 15:40:59 +0000

    Housekeeping

---
+++ ZohoApi/zoho.cpp
@@ -0,0 +1,49 @@
+#include "stdafx.h"
+#include "zoho.h"
+
+#include <vector>
+#include <sstream>
+#include <numeric>
+
+const std::string zoho::base_url = "https://projectsapi.zoho.com/restapi";
+const std::string zoho::client_id = "1000.0ESNDLDTNIT733894WBY544KIGZAEA";
+const std::string zoho::client_secret = "15300e4a0f75ec7f400e7442c3bb5ccd250e1e2a8b";
+const std::string zoho::scopes[] = {
+	"ZohoProjects.portals.READ",
+	"ZohoProjects.projects.ALL",
+	"ZohoProjects.activities.READ",
+	"ZohoProjects.milestones.ALL",
+	"ZohoProjects.tasklists.ALL",
+	"ZohoProjects.tasks.ALL",
+	"ZohoProjects.timesheets.ALL",
+	"ZohoProjects.bugs.ALL",
+	"ZohoProjects.events.ALL",
+	"ZohoProjects.forums.ALL",
+	"ZohoProjects.users.ALL",
+};
+const std::string zoho::redirect_url = "http://localhost:8702";
+const std::string zoho::accounts_url = "https://accounts.zoho.com";
+
+zoho::zoho()
+{
+}
+
+std::string zoho::authorization_url()
+{
+	std::ostringstream oss;
+	oss << "https://accounts.zoho.com/oauth/v2/auth";
+	oss << "?scope=" << std::accumulate(std::begin(scopes), std::end(scopes), std::string(),
+		[](const std::string& a, const std::string& b) -> std::string {
+			return a + (a.length() > 0 ? "," : "") + b;
+		});
+	oss << "&access_type=" << "offline";
+	oss << "&prompt=" << "consent";
+	return oss.str();
+}
+
+std::string zoho::token_url()
+{
+	std::ostringstream oss;
+	oss << "https://accounts.zoho.com/oauth/v2/token";
+	return oss.str();
+}
---
+++ ZohoApi/zoho.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+
+class zoho
+{
+private:
+	static const std::string scopes[];
+
+	zoho();
+public:
+	static std::string authorization_url();
+	static std::string token_url();
+
+	static const std::string base_url;
+	static const std::string client_id;
+	static const std::string client_secret;
+	static const std::string redirect_url;
+	static const std::string accounts_url;
+};
+
