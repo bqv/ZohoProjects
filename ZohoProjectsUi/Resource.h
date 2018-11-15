ec1ecd01aeef95cfdefd18e61a348463711f5768
Tony Olagbaiye <frony0@gmail.com>
2018-11-15 01:10:57 +0000

    Bugfixes 1/1

--- ZohoProjectsUi/Resource.h
+++ ZohoProjectsUi/Resource.h
@@ -6,17 +6,14 @@
 #define IDS_PRODUCTDETAILS              102
 #define IDS_COMPANYNAME                 103
 #define IDS_VERSION                     104
-#define IDS_WINDOW_TITLE                120
+#define IDS_ProjectExplorer_Title                120
 #define IDS_E_CANTCREATETOOL            121
 #define IDS_E_GETVIEWOBJECT_CALLED_AGAIN 122
 #define IDS_BUTTONCLICK_MESSAGE         123
 #define IDB_IMAGES                      300
 #define IDI_PACKAGE_ICON                400
 #define IDB_BITMAP1                     404
-#define IDD_ZohoProjects_DLG            800
 #define IDD_ZohoProjects_ProjectExplorer 800
-#define IDC_BROWSER                     1000
-#define IDC_ProjectTree                 1001
 
 // Next default values for new objects
 // 
@@ -24,7 +21,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        405
 #define _APS_NEXT_COMMAND_VALUE         40002
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           402
 #endif
 #endif
